3ff43eb90f1e317a47df2979d28bf11c9cdbb1c5
Nick Bodkins <nickbodkins@Mac.localdomain>
2026-02-02 14:35:04 -0500

    chore: Add RFI and submittal module exports

---
+++ apps/web/lib/rfis/index.ts
@@ -0,0 +1,12 @@
+export {
+  RFI_STATUS_LABELS,
+  RFI_STATUS_COLORS,
+  RFI_PRIORITY_LABELS,
+  RFI_PRIORITY_COLORS,
+  calculateDaysOpen,
+  isOverdue,
+  sortRFIs,
+  filterRFIs,
+  generateRFINumber,
+  getRFISummary,
+} from './utils';
---
+++ apps/web/lib/submittals/index.ts
@@ -0,0 +1,13 @@
+export {
+  SUBMITTAL_STATUS_LABELS,
+  SUBMITTAL_STATUS_COLORS,
+  SUBMITTAL_TYPE_LABELS,
+  SUBMITTAL_TYPE_ICONS,
+  calculateReviewDays,
+  isApproved,
+  needsAction,
+  sortSubmittals,
+  filterSubmittals,
+  generateSubmittalNumber,
+  getSubmittalSummary,
+} from './utils';
