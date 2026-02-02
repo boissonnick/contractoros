2405b818b056803ffae68ea6720ce44533984dc0
Nick Bodkins <nickbodkins@Mac.localdomain>
2026-02-02 14:18:09 -0500

    chore: Add punch list and closeout module exports

---
+++ apps/web/lib/closeout/index.ts
@@ -0,0 +1,5 @@
+export {
+  DEFAULT_CLOSEOUT_ITEMS,
+  createCloseoutChecklist,
+  calculateCloseoutProgress,
+} from './checklist-templates';
---
+++ apps/web/lib/punch-list/index.ts
@@ -0,0 +1,10 @@
+export {
+  PUNCH_STATUS_LABELS,
+  PUNCH_STATUS_COLORS,
+  PUNCH_PRIORITY_LABELS,
+  PUNCH_PRIORITY_COLORS,
+  PUNCH_CATEGORIES,
+  sortPunchItems,
+  filterPunchItems,
+  getPunchListSummary,
+} from './utils';
